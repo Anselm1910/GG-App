fd26b33d32ecd4c756851b3a7ce2dab43e8b5e27
Anselm1910 <anselmmembis@gmail.com>
2019-05-28 14:31:32 +0100

    my first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+const http = require('http')
+
+const port = 5000
+
+// const fs = require('fs')
+
+// const data = fs.readFileSync('hello.txt')
+// console.log(data.toString())
+
+http.createServer(function(request, response){
+ response.write('Welcome to this server')
+response.end()
+}).listen(port)
+
+console.log(`server is runing at ${port}`)
