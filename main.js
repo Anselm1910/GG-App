b0359598dba43e7c470b2f772b6bcf51d5f2d383
Anselm1910 <anselmmembis@gmail.com>
2019-05-28 15:03:45 +0100

    my second commit

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ const http = require('http')
 
 const port = 5000
 
-// const fs = require('fs')
+ const fs = require('fs')
 
-// const data = fs.readFileSync('hello.txt')
-// console.log(data.toString())
+const data = fs.readFileSync('hello.txt')
+console.log(data.toString())
 
 http.createServer(function(request, response){
  response.write('Welcome to this server')
